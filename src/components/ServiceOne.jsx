76074fa71d1d087bae8c28b0bf76df1404cca288
saalmansourav6122 <saalmansourav111@gmail.com>
2025-05-10 16:54:40 +0600

    sub service one done

--- src/components/ServiceOne.jsx
+++ src/components/ServiceOne.jsx
@@ -3,10 +3,15 @@ import SubService from "./SubService";
 import feature1 from "../assets/features/feature-1.png";
 
 function ServiceOne() {
+  const subSerOneList = [
+    "Continuous integration and deployment",
+    "Development workflow",
+    "Knowledge management",
+  ];
   return (
     <>
-      <section class="bg-gray-50 dark:bg-gray-800">
-        <div class="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
+      <section className="bg-gray-50 dark:bg-gray-800">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
           <SubService
             title="Work with tools you already use"
             paragraph=" Deliver great service experiences fast - without the complexity of
@@ -15,6 +20,8 @@ function ServiceOne() {
             lastParagraph="Deliver great service experiences fast - without the complexity of
             traditional ITSM solutions."
             img={feature1}
+            alt="dashboard feature image"
+            subSerOneList={subSerOneList}
           />
         </div>
       </section>

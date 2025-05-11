e05bac8298b4539bcc560659603df606ef227dab
saalmansourav6122 <saalmansourav111@gmail.com>
2025-05-11 10:23:28 +0600

    completed service one

--- src/components/ServiceOne.jsx
+++ src/components/ServiceOne.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import SubService from "./SubService";
 import feature1 from "../assets/features/feature-1.png";
+import feature2 from "../assets/features/feature-2.png";
 
 function ServiceOne() {
+  const subSerTwoSty = "flex-row-reverse";
   const subSerOneList = [
     "Continuous integration and deployment",
     "Development workflow",
+    "Knowledge management",
+  ];
+  const subSerTwoList = [
+    "Dynamic reports and dashboards",
+    "Templates for everyone",
+    "Development workflow",
+    "Limitless business automation",
     "Knowledge management",
   ];
   return (
@@ -21,7 +30,16 @@ function ServiceOne() {
             traditional ITSM solutions."
             img={feature1}
             alt="dashboard feature image"
-            subSerOneList={subSerOneList}
+            subSereList={subSerOneList}
+          />
+          <SubService
+            title="We invest in the world’s potential"
+            paragraph="  Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease."
+            lastParagraph="Deliver great service experiences fast - without the complexity of traditional ITSM solutions."
+            subSerTwoSty={subSerTwoSty}
+            img={feature2}
+            alt="dashboard feature image"
+            subSereList={subSerTwoList}
           />
         </div>
       </section>
--- src/components/SubService.jsx
+++ src/components/SubService.jsx
@@ -7,12 +7,13 @@ export default function SubService({
   lastParagraph,
   img,
   alt,
-  subSerOneList,
+  subSereList,
+  subSerTwoSty,
 }) {
   return (
     <>
-      <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
-        <div className="text-gray-500 sm:text-lg dark:text-gray-400">
+      <div className={`items-center justify-between flex ${subSerTwoSty}`}>
+        <div className="text-red-500 sm:text-lg dark:text-gray-400 w-1/2">
           <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
             {title}
           </h2>
@@ -22,7 +23,7 @@ export default function SubService({
             role="list"
             className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700"
           >
-            {subSerOneList.map((list, id) => (
+            {subSereList.map((list, id) => (
               <li key={list + id} className="flex space-x-3">
                 <CirclewithTick />
                 <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
@@ -35,7 +36,7 @@ export default function SubService({
           <p className="mb-8 font-light lg:text-xl">{lastParagraph}</p>
         </div>
         <img
-          className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex"
+          className="hidden w-1/2 mb-4 rounded-lg lg:mb-0 lg:flex"
           src={img}
           alt={alt}
         />
